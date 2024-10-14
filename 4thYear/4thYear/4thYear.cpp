345492a2ef93d5b548bae4fca783af61c97297a8
Josh <joshua.boycehyland15@gmail.com>
2024-10-14 20:23:40 +0100

    got 3d tutorial working

--- 4thYear/4thYear/4thYear.cpp
+++ 4thYear/4thYear/4thYear.cpp
@@ -1,19 +1,20 @@
 /*******************************************************************************************
 *
-*   raylib [core] example - 2D Camera system
+*   raylib [core] example - 3d camera first person
 *
-*   Example originally created with raylib 1.5, last time updated with raylib 3.0
+*   Example originally created with raylib 1.3, last time updated with raylib 1.3
 *
 *   Example licensed under an unmodified zlib/libpng license, which is an OSI-certified,
 *   BSD-like license that allows static linking with closed source software
 *
-*   Copyright (c) 2016-2024 Ramon Santamaria (@raysan5)
+*   Copyright (c) 2015-2024 Ramon Santamaria (@raysan5)
 *
 ********************************************************************************************/
 
 #include "raylib.h"
 
-#define MAX_BUILDINGS   100
+
+#define MAX_COLUMNS 20
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -25,35 +26,31 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - 3d camera first person");
 
-    Rectangle player = { 400, 280, 40, 40 };
-    Rectangle buildings[MAX_BUILDINGS] = { 0 };
-    Color buildColors[MAX_BUILDINGS] = { 0 };
+    // Define the camera to look into our 3d world (position, target, up vector)
+    Camera camera = { 0 };
+    camera.position = { 0.0f, 2.0f, 4.0f };    // Camera position
+    camera.target = { 0.0f, 2.0f, 0.0f };      // Camera looking at point
+    camera.up = { 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 60.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera projection type
 
-    int spacing = 0;
+    int cameraMode = CAMERA_FIRST_PERSON;
 
-    for (int i = 0; i < MAX_BUILDINGS; i++)
+    // Generates some random columns
+    float heights[MAX_COLUMNS] = { 0 };
+    Vector3 positions[MAX_COLUMNS] = { 0 };
+    Color colors[MAX_COLUMNS] = { 0 };
+
+    for (int i = 0; i < MAX_COLUMNS; i++)
     {
-        buildings[i].width = (float)GetRandomValue(50, 200);
-        buildings[i].height = (float)GetRandomValue(100, 800);
-        buildings[i].y = screenHeight - 130.0f - buildings[i].height;
-        buildings[i].x = -6000.0f + spacing;
-
-        spacing += (int)buildings[i].width;
-
-        //buildColors[i] = (Color){ GetRandomValue(200, 240), GetRandomValue(200, 240), GetRandomValue(200, 250), 255 };
-        buildColors[i].a = GetRandomValue(200, 240);
-        buildColors[i].b = GetRandomValue(200, 240);
-        buildColors[i].g = GetRandomValue(200, 250);
-        buildColors[i].r = 255;
+        heights[i] = (float)GetRandomValue(1, 12);
+        positions[i] = { (float)GetRandomValue(-15, 15), heights[i] / 2.0f, (float)GetRandomValue(-15, 15) };
+        colors[i] ={ (unsigned char)GetRandomValue(20, 255), (unsigned char)GetRandomValue(10, 55), 30, 255 };
     }
 
-    Camera2D camera = { 0 };
-    camera.target = { player.x + 20.0f, player.y + 20.0f };
-    camera.offset = { screenWidth/2.0f, screenHeight/2.0f };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
+    DisableCursor();                    // Limit cursor to relative movement inside the window
 
     SetTargetFPS(60);                   // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -63,69 +60,138 @@ int main(void)
     {
         // Update
         //----------------------------------------------------------------------------------
-        // Player movement
-        if (IsKeyDown(KEY_RIGHT)) player.x += 2;
-        else if (IsKeyDown(KEY_LEFT)) player.x -= 2;
-
-        // Camera target follows player
-        camera.target = { player.x + 20, player.y + 20 };
-
-        // Camera rotation controls
-        if (IsKeyDown(KEY_A)) camera.rotation--;
-        else if (IsKeyDown(KEY_S)) camera.rotation++;
-
-        // Limit camera rotation to 80 degrees (-40 to 40)
-        if (camera.rotation > 40) camera.rotation = 40;
-        else if (camera.rotation < -40) camera.rotation = -40;
-
-        // Camera zoom controls
-        camera.zoom += ((float)GetMouseWheelMove()*0.05f);
-
-        if (camera.zoom > 3.0f) camera.zoom = 3.0f;
-        else if (camera.zoom < 0.1f) camera.zoom = 0.1f;
-
-        // Camera reset (zoom and rotation)
-        if (IsKeyPressed(KEY_R))
+        // Switch camera mode
+        if (IsKeyPressed(KEY_ONE))
         {
-            camera.zoom = 1.0f;
-            camera.rotation = 0.0f;
+            cameraMode = CAMERA_FREE;
+            camera.up = { 0.0f, 1.0f, 0.0f }; // Reset roll
         }
+
+        if (IsKeyPressed(KEY_TWO))
+        {
+            cameraMode = CAMERA_FIRST_PERSON;
+            camera.up = { 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
+
+        if (IsKeyPressed(KEY_THREE))
+        {
+            cameraMode = CAMERA_THIRD_PERSON;
+            camera.up = { 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
+
+        if (IsKeyPressed(KEY_FOUR))
+        {
+            cameraMode = CAMERA_ORBITAL;
+            camera.up = { 0.0f, 1.0f, 0.0f }; // Reset roll
+        }
+
+        // Switch camera projection
+        if (IsKeyPressed(KEY_P))
+        {
+            if (camera.projection == CAMERA_PERSPECTIVE)
+            {
+                // Create isometric view
+                cameraMode = CAMERA_THIRD_PERSON;
+                // Note: The target distance is related to the render distance in the orthographic projection
+                camera.position = { 0.0f, 2.0f, -100.0f };
+                camera.target = { 0.0f, 2.0f, 0.0f };
+                camera.up = { 0.0f, 1.0f, 0.0f };
+                camera.projection = CAMERA_ORTHOGRAPHIC;
+                camera.fovy = 20.0f; // near plane width in CAMERA_ORTHOGRAPHIC
+          /*      CameraYaw(&camera, -135 * DEG2RAD, true);
+                CameraPitch(&camera, -45 * DEG2RAD, true, true, false);*/
+            }
+            else if (camera.projection == CAMERA_ORTHOGRAPHIC)
+            {
+                // Reset to default view
+                cameraMode = CAMERA_THIRD_PERSON;
+                camera.position = { 0.0f, 2.0f, 10.0f };
+                camera.target = { 0.0f, 2.0f, 0.0f };
+                camera.up = { 0.0f, 1.0f, 0.0f };
+                camera.projection = CAMERA_PERSPECTIVE;
+                camera.fovy = 60.0f;
+            }
+        }
+
+        // Update camera computes movement internally depending on the camera mode
+        // Some default standard keyboard/mouse inputs are hardcoded to simplify use
+        // For advance camera controls, it's reecommended to compute camera movement manually
+        UpdateCamera(&camera, cameraMode);                  // Update camera
+
+        /*
+                // Camera PRO usage example (EXPERIMENTAL)
+                // This new camera function allows custom movement/rotation values to be directly provided
+                // as input parameters, with this approach, rcamera module is internally independent of raylib inputs
+                UpdateCameraPro(&camera,
+                    (Vector3){
+                        (IsKeyDown(KEY_W) || IsKeyDown(KEY_UP))*0.1f -      // Move forward-backward
+                        (IsKeyDown(KEY_S) || IsKeyDown(KEY_DOWN))*0.1f,
+                        (IsKeyDown(KEY_D) || IsKeyDown(KEY_RIGHT))*0.1f -   // Move right-left
+                        (IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT))*0.1f,
+                        0.0f                                                // Move up-down
+                    },
+                    (Vector3){
+                        GetMouseDelta().x*0.05f,                            // Rotation: yaw
+                        GetMouseDelta().y*0.05f,                            // Rotation: pitch
+                        0.0f                                                // Rotation: roll
+                    },
+                    GetMouseWheelMove()*2.0f);                              // Move to target (zoom)
+        */
         //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            BeginMode2D(camera);
+        BeginMode3D(camera);
 
-                DrawRectangle(-6000, 320, 13000, 8000, DARKGRAY);
+        DrawPlane({ 0.0f, 0.0f, 0.0f }, { 32.0f, 32.0f }, LIGHTGRAY); // Draw ground
+        DrawCube({ -16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, BLUE);     // Draw a blue wall
+        DrawCube({ 16.0f, 2.5f, 0.0f }, 1.0f, 5.0f, 32.0f, LIME);      // Draw a green wall
+        DrawCube({ 0.0f, 2.5f, 16.0f }, 32.0f, 5.0f, 1.0f, GOLD);      // Draw a yellow wall
 
-                for (int i = 0; i < MAX_BUILDINGS; i++) DrawRectangleRec(buildings[i], buildColors[i]);
+        // Draw some cubes around
+        for (int i = 0; i < MAX_COLUMNS; i++)
+        {
+            DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+            DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+        }
 
-                DrawRectangleRec(player, RED);
+        // Draw player cube
+        if (cameraMode == CAMERA_THIRD_PERSON)
+        {
+            DrawCube(camera.target, 0.5f, 0.5f, 0.5f, PURPLE);
+            DrawCubeWires(camera.target, 0.5f, 0.5f, 0.5f, DARKPURPLE);
+        }
 
-                DrawLine((int)camera.target.x, -screenHeight*10, (int)camera.target.x, screenHeight*10, GREEN);
-                DrawLine(-screenWidth*10, (int)camera.target.y, screenWidth*10, (int)camera.target.y, GREEN);
+        EndMode3D();
 
-            EndMode2D();
+        // Draw info boxes
+        DrawRectangle(5, 5, 330, 100, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(5, 5, 330, 100, BLUE);
 
-            DrawText("SCREEN AREA", 640, 10, 20, RED);
+        DrawText("Camera controls:", 15, 15, 10, BLACK);
+        DrawText("- Move keys: W, A, S, D, Space, Left-Ctrl", 15, 30, 10, BLACK);
+        DrawText("- Look around: arrow keys or mouse", 15, 45, 10, BLACK);
+        DrawText("- Camera mode keys: 1, 2, 3, 4", 15, 60, 10, BLACK);
+        DrawText("- Zoom keys: num-plus, num-minus or mouse scroll", 15, 75, 10, BLACK);
+        DrawText("- Camera projection key: P", 15, 90, 10, BLACK);
 
-            DrawRectangle(0, 0, screenWidth, 5, RED);
-            DrawRectangle(0, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(screenWidth - 5, 5, 5, screenHeight - 10, RED);
-            DrawRectangle(0, screenHeight - 5, screenWidth, 5, RED);
+        DrawRectangle(600, 5, 195, 100, Fade(SKYBLUE, 0.5f));
+        DrawRectangleLines(600, 5, 195, 100, BLUE);
 
-            DrawRectangle( 10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
-            DrawRectangleLines( 10, 10, 250, 113, BLUE);
-
-            DrawText("Free 2d camera controls:", 20, 20, 10, BLACK);
-            DrawText("- Right/Left to move Offset", 40, 40, 10, DARKGRAY);
-            DrawText("- Mouse Wheel to Zoom in-out", 40, 60, 10, DARKGRAY);
-            DrawText("- A / S to Rotate", 40, 80, 10, DARKGRAY);
-            DrawText("- R to reset Zoom and Rotation", 40, 100, 10, DARKGRAY);
+        DrawText("Camera status:", 610, 15, 10, BLACK);
+        DrawText(TextFormat("- Mode: %s", (cameraMode == CAMERA_FREE) ? "FREE" :
+            (cameraMode == CAMERA_FIRST_PERSON) ? "FIRST_PERSON" :
+            (cameraMode == CAMERA_THIRD_PERSON) ? "THIRD_PERSON" :
+            (cameraMode == CAMERA_ORBITAL) ? "ORBITAL" : "CUSTOM"), 610, 30, 10, BLACK);
+        DrawText(TextFormat("- Projection: %s", (camera.projection == CAMERA_PERSPECTIVE) ? "PERSPECTIVE" :
+            (camera.projection == CAMERA_ORTHOGRAPHIC) ? "ORTHOGRAPHIC" : "CUSTOM"), 610, 45, 10, BLACK);
+        DrawText(TextFormat("- Position: (%06.3f, %06.3f, %06.3f)", camera.position.x, camera.position.y, camera.position.z), 610, 60, 10, BLACK);
+        DrawText(TextFormat("- Target: (%06.3f, %06.3f, %06.3f)", camera.target.x, camera.target.y, camera.target.z), 610, 75, 10, BLACK);
+        DrawText(TextFormat("- Up: (%06.3f, %06.3f, %06.3f)", camera.up.x, camera.up.y, camera.up.z), 610, 90, 10, BLACK);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
